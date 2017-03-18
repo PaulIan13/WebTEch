d90ac5d4f6a458d5a8940d9f6e8f9e78fa6b0251
Donglawen <2150509@slu.edu.ph>
2017-03-18 15:25:24 +0800

    HTML of the course site first version

---
+++ prelimlec/js/script.js
@@ -0,0 +1,19 @@
+
+// Get the moda
+function show() {
+var modal = document.getElementById('prelims');
+var btn = document.getElementById('showPrelim');
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+}
+
